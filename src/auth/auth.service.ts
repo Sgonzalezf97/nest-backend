aac92b59c8d8dff5eef483bf2a8fb69ead7d84c6
Sebastian Gonzalez <jusegofo@gmail.com>
2024-09-10 22:53:17 -0500

    [New] validating the JWT to login and creeating a guard to validate user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,12 @@
 import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
 import { Model } from 'mongoose';
 import * as bcryptjs from 'bcryptjs';
-import { LoginDto } from './dto/login.dto';
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from './interfaces/jwt-payload';
+import { LoginResponse } from './interfaces/login-response';
+import{CreateUserDto,UpdateAuthDto,LoginDto,RegisterDto} from './dto/index'
 
 @Injectable()
 export class AuthService {
@@ -19,48 +18,53 @@ export class AuthService {
   ){
 
   }
+
+  //Método asincrono para crear un usuario 
   async create(createUserDto: CreateUserDto): Promise<User> {
-
     try {
-
+      console.log(createUserDto)
       const {password, ...userData} = createUserDto;
-
       const newUser = new this.userModel({
         password: bcryptjs.hashSync(password,10),
         ...userData
       })
       await newUser.save();
       const { password:_, ...user}= newUser.toJSON();
+      console.log({user})
       return user
 
     } catch (error) {
       if (error.code === 11000) {
-        throw new BadRequestException(`${ createUserDto.email} already exists!`)
+        throw new BadRequestException(`${ createUserDto.email} already exists! ${ error}`)
       }
       throw new InternalServerErrorException('Something went wrong')
     }
-    // console.log(createUserDto);
-    // const newUser = new this.userModel(createUserDto)
-    // return newUser.save();
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async findUserById(id: string){
+    const user = await this.userModel.findById(id);
+    const {password, ...rest}= user.toJSON();
+    return rest
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  findAll() : Promise <User[]>{
+    return this.userModel.find();
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} auth`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+  // update(id: number, updateAuthDto: UpdateAuthDto) {
+  //   return `This action updates a #${id} auth`;
+  // }
 
-  async login(loginDto : LoginDto){
+  // remove(id: number) {
+  //   return `This action removes a #${id} auth`;
+  // }
+
+  //Método para loggearse por medio de usuario y contraseña, generando un JWT en caso de inciar sesión
+  async login(loginDto : LoginDto): Promise<LoginResponse>{
     console.log({loginDto})
     const { email, password} = loginDto;
     const user = await this.userModel.findOne({email})
@@ -73,7 +77,6 @@ export class AuthService {
       throw new UnauthorizedException("Not valid credentials");
     }
 
-
     const {password:_, ...rest} = user.toJSON();
     return{
       user:rest,
@@ -81,8 +84,20 @@ export class AuthService {
     }
   }
 
+  //Método para generar un JWT usando como parametro un payload que hace referencia al id del usuario
   getJwtToken(payload: JwtPayload){
     const token = this.jwtService.sign(payload);
     return token;
+  }
+
+  //Método para registrar un usuario, este lo crea y genera el JWT 
+  async register(registerDto:RegisterDto): Promise<LoginResponse>{
+
+    const user = await this.create(registerDto);
+    console.log({user});
+    return{
+      user: user,
+      token: this.getJwtToken({id:user._id})
+    }
   }
 }
